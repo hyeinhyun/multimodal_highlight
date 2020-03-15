6dc7e8070095a1456f4c3e46d4c601f10321d053
Radominic <wjdrmf314@gmail.com>
2020-03-15 16:27:50 +0900

    caption processing

---
+++ speech_to_text/caption_processing.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 15 15:04:19 2020
+
+@author: Gangmin
+"""
+
+import pandas as pd
+import re
+import nltk
+from nltk.corpus import stopwords
+from collections import OrderedDict
+from datetime import datetime, timedelta
+
+
+
+def read(file_name):
+    tempLine=[]
+    vocab=OrderedDict()
+    #nltk.download('stopwords')
+
+    
+    with open(file_name,encoding='utf8') as f:
+        #default_time=f.readlines()[1].split(']')[0][1:]
+        textt = ""
+        lineNot = 0
+        check = False
+        for lineNo,line in enumerate(f.readlines()[1:]):
+            if(lineNo%2 == 1):
+                text=line
+                text=re.sub(r'\<[^)]*\>', '', text)
+                text=re.sub('[^a-zA-Z]', ' ', text)
+                text = text.strip()
+                text=text.lower()
+                
+                #print(text)
+                stops = set(stopwords.words('english'))
+                
+                
+                if not text in stops:
+                    check = True
+                    textt = text
+                    lineNot = lineNo
+                    
+                    
+            if(  check == True and lineNo%2 == 0):
+                line = line.rstrip('\n') 
+                temp={"time":line,"text":textt,"lineno":lineNot+1}
+                tempLine.append(temp)
+                check = False
+                #print(temp["time"])
+                if temp['text'] not in vocab:
+                        vocab[temp['text']]=0
+        
+    #print(tempLine)
+    print(vocab)
+    return tempLine,vocab
+
+
+
+if __name__ == '__main__':
+    
+    read('C:/Users/Gangmin/Desktop/gangmin/캡스톤/tsvt/speech_to_text/stt_result.txt')
